abda24860aece4494af7537fb97a9933760d7aa0
shivani12-12 <78165124+shivani12-12@users.noreply.github.com>
2021-08-08 20:10:03 +0700

    Uploading files
    HTML, CSS, JS

---
+++ questions.js
@@ -0,0 +1,58 @@
+// storing in array questions and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Which data structure follows LIFO order?",
+        answer: "Stack",
+        options: [
+            "Array",
+            "Stack",
+            "Queue",
+            "Linked List"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which data structure follows FIFO order?",
+        answer: "Queue",
+        options: [
+            "Array",
+            "Stack",
+            "Queue",
+            "Linked List"
+        ]
+    },
+    {
+        numb: 3,
+        question: "How many layers are there in OSI Model?",
+        answer: "7",
+        options: [
+            "4",
+            "5",
+            "7",
+            "8"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SDLC stands for?",
+        answer: "Software Development Life Cycle",
+        options: [
+            "System Development Life Cycle",
+            "Secure Development Life Cycle",
+            "System Deployment Life Cycle",
+            "Software Development Life Cycle",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Rows of a relation are known as the __________.",
+        answer: "Tuples",
+        options: [
+            "Degree",
+            "Tuples",
+            "Entity",
+            "cardinality"
+        ]
+    }
+]
